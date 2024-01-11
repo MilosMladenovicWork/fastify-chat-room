897ae4e9bbd943514e514ef6fc7e72524bc4a623
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2024-01-11 20:54:23 +0100

    feat(join-room.listener): :sparkles: save room in state on join_room event

---
+++ src/socket-event-listeners/join-room.listener.ts
@@ -0,0 +1,57 @@
+import { Server, Socket } from "socket.io";
+import { rooms } from "../state/rooms";
+import {
+  ClientToServerEvents,
+  InterServerEvents,
+  ServerToClientEvents,
+  SocketData,
+} from "../types/socketio.types";
+
+export const joinRoomListener = ({
+  socket,
+  socketIo,
+}: {
+  socket: Socket<
+    ClientToServerEvents,
+    ServerToClientEvents,
+    InterServerEvents,
+    SocketData
+  >;
+  socketIo: Server<
+    ClientToServerEvents,
+    ServerToClientEvents,
+    InterServerEvents,
+    SocketData
+  >;
+}) => {
+  socket.on("join_room", ({ roomName }) =>
+    joinRoomListenerHandler({ socket, socketIo }, { roomName })
+  );
+};
+
+const joinRoomListenerHandler = (
+  {
+    socket,
+    socketIo,
+  }: {
+    socket: Socket<
+      ClientToServerEvents,
+      ServerToClientEvents,
+      InterServerEvents,
+      SocketData
+    >;
+    socketIo: Server<
+      ClientToServerEvents,
+      ServerToClientEvents,
+      InterServerEvents,
+      SocketData
+    >;
+  },
+  { roomName }: { roomName: string }
+) => {
+  if (!rooms.includes(roomName)) {
+    rooms.push(roomName);
+  }
+
+  socket.join(roomName);
+};
