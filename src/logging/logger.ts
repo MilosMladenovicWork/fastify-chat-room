dfd9360de2c74051d2236e7825db71928b79ed39
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2024-01-14 13:18:40 +0100

    feat(winston): :sparkles: register elasticsearch transport

--- src/logging/logger.ts
+++ src/logging/logger.ts
@@ -1,9 +1,24 @@
 import winston from "winston";
+import {
+  ElasticsearchTransport,
+  ElasticsearchTransportOptions,
+} from "winston-elasticsearch";
+import { fastifyElasticsearchConfig } from "../config/fastify-elasticsearch.config";
+
+const esTransportOpts: ElasticsearchTransportOptions = {
+  level: "info",
+  clientOpts: {
+    node: fastifyElasticsearchConfig.node,
+  },
+};
+
+const esTransport = new ElasticsearchTransport(esTransportOpts);
 
 export const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
   transports: [
+    esTransport,
     new winston.transports.File({ filename: "error.log", level: "error" }),
     new winston.transports.File({ filename: "combined.log" }),
   ],
