c228dd7e7c63e37777997848498025ea82ff8c03
MilosMladenovicWork <milos.mladenovic.work@gmail.com>
2024-01-14 12:52:39 +0100

    feat(elasticsearch): :sparkles: add fastify elasticsearch plugin to fastify app

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import fastify from "fastify";
 import { fastifySocketIoPlugin } from "./plugins/fastify-socketio.plugin";
 
+import fastifyElasticsearch from "@fastify/elasticsearch";
 import fastifyStatic from "@fastify/static";
 import { socketIoBootstrap } from "./bootstrap/socketio.bootstrap";
+import { fastifyElasticsearchConfig } from "./config/fastify-elasticsearch.config";
 import { fastifyStaticConfig } from "./config/fastify-static.config";
 import { logger } from "./logging/logger";
 
@@ -10,6 +12,7 @@ const app = fastify();
 
 app.register(fastifySocketIoPlugin);
 app.register(fastifyStatic, fastifyStaticConfig);
+app.register(fastifyElasticsearch, fastifyElasticsearchConfig);
 
 app.listen({ host: "0.0.0.0", port: 3000 });
 
